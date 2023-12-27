4565590501db43fde10c930f63f22b16ea465e5d
SamuelClauss <69733214+SamuelClauss@users.noreply.github.com>
2023-12-27 23:49:30 +0100

    Update RCTContractInterface.sol

--- RCTContractInterface.sol
+++ RCTContractInterface.sol
@@ -1,1 +1,18 @@
-..
+// ***************************************************************************************************************
+// SPDX-License-Identifier: MIT
+// @title RCTContractInterface
+// @authors Samuel Clauss
+// Smart Contracts Lab, University of Zurich
+// Created: December 18, 2023
+// ***************************************************************************************************************
+// Read the Whitepaper https://github.com/SCL-Project/Regulatory
+// ***************************************************************************************************************
+pragma solidity ^0.8.20;
+
+interface RCTContractInterface {
+    function getCompany(address) external view returns(string memory, string memory, string memory);
+    function transferNFT(address, uint256) external;
+    function getTokenInformation(uint256) external view returns(uint8, uint32, uint40, uint8, string memory, string memory);
+    function ownerOf(uint256 tokenId) external view returns (address owner);
+    function getAddresses(uint _tokenId) external  view returns(address[] memory);
+}
